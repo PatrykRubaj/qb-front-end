4588914674bd52644a18b9a76f8069c70830c937
Patryk Rubaj <patryk.rubaj@gmail.com>
2020-05-02 13:31:28 +0200

    working on array of fields validation with formik

--- src/SpreadsheetGeneration/SpendingPrediction/SpendingPredictionComponent.tsx
+++ src/SpreadsheetGeneration/SpendingPrediction/SpendingPredictionComponent.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Category, Subcategory } from "../state";
+import { Formik, Form, Field, FieldArray } from "formik";
+import { render } from "@testing-library/react";
+import * as Yup from "yup";
 
 interface Props {
   categories: Array<Category>;
@@ -21,11 +24,24 @@ const SpendingPredictionComponent: React.FC<Props> = ({
     enterSubcategoryAmount(subcategory.id, +value);
   };
 
+  const schema = Yup.object().shape({
+    subcategories: Yup.array()
+      .of(
+        Yup.object().shape({
+          amount: Yup.number()
+            .typeError("Amount must be a number")
+            .required("Amount is required")
+            .min(0, "Amount must be >= 0")
+        })
+      )
+      .required("Must have subcategories") // these constraints are shown if and only if inner constraints are satisfied
+  });
+
   return (
     <div className="row">
       <div className="col">
         <h2>Spending plan</h2>
-        <form>
+        {/* <form>
           {categories.map(category => (
             <React.Fragment key={category.id}>
               <h3>{category.name}</h3>
@@ -46,7 +62,59 @@ const SpendingPredictionComponent: React.FC<Props> = ({
                 ))}
             </React.Fragment>
           ))}
-        </form>
+        </form> */}
+
+        {categories.map((category, catIndex) => (
+          <Formik
+            key={category.id}
+            enableReinitialize={true}
+            isInitialValid={true}
+            validateOnBlur={true}
+            initialValues={{
+              subcategories: subcategories.filter(
+                x => x.categoryId === category.id
+              )
+            }}
+            onSubmit={(values): void => {
+              console.log("sent ", values);
+            }}
+            validationSchema={schema}
+            render={({ values, errors }) => (
+              <Form>
+                <h3>{category.name}</h3>
+                {console.log(errors)}
+                <FieldArray
+                  name={`subcategories`}
+                  render={arrayHelpers => (
+                    <>
+                      {values.subcategories.map((subcategory, index) => {
+                        console.log("index: ", index, "subcaT:", subcategory);
+                        return (
+                          <div className="form-group" key={`${subcategory.id}`}>
+                            <label htmlFor="formGroupExampleInput">{`Subcategory ${subcategory.name}`}</label>
+                            {/* <input
+                              name="amount"
+                              type="text"
+                              className="form-control"
+                              placeholder={`Subcategory ${subcategory.name}`}
+                              onChange={(e): void =>
+                                onInputChange(e, subcategory)
+                              }
+                            /> */}
+                            <Field
+                              name={`subcategories.${index}.amount`}
+                              className="form-control"
+                            />
+                          </div>
+                        );
+                      })}
+                    </>
+                  )}
+                />
+              </Form>
+            )}
+          />
+        ))}
       </div>
     </div>
   );
