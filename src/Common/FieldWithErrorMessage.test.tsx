34c7484bbfab35e4c80ca64e45ec4bae91e42fb5
Patryk Rubaj <patryk.rubaj@gmail.com>
2020-05-07 15:28:58 +0200

    setup for tests

---
+++ src/Common/FieldWithErrorMessage.test.tsx
@@ -0,0 +1,7 @@
+import FieldWithErrorMessage from "./FieldWithErrorMessage";
+
+describe(`The Field with Error Message component`, () => {
+  it(`Should not regress`, () => {
+    expect(40 + 2).toEqual(42);
+  });
+});
