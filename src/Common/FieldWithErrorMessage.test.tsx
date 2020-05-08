9437c46f6c5f725211aa5f9b174b674b8054443f
Patryk Rubaj <patryk.rubaj@gmail.com>
2020-05-08 15:09:27 +0200

    refactored FieldWithErrorMessage to expose pure component easier for testing

--- src/Common/FieldWithErrorMessage.test.tsx
+++ src/Common/FieldWithErrorMessage.test.tsx
@@ -1,7 +1,29 @@
+import React from "react";
 import FieldWithErrorMessage from "./FieldWithErrorMessage";
+import renderer from "react-test-renderer";
 
 describe(`The Field with Error Message component`, () => {
-  it(`Should not regress`, () => {
-    expect(40 + 2).toEqual(42);
+  it(`Should renders successfuly`, () => {
+    const name = "email";
+    const value = "patryk.pl";
+    const onChange = (): void => {};
+    const onBlur = (): void => {};
+    const touch = true;
+    const error = "Invalid email address";
+
+    const tree = renderer
+      .create(
+        <FieldWithErrorMessage
+          name={name}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          touch={touch}
+          error={error}
+        />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
