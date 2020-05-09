d40b20f938fcc0de6a715b1835ab06de71def572
Patryk Rubaj <patryk.rubaj@gmail.com>
2020-05-09 13:37:00 +0200

    updated packages and fixed React complainging about jest version

--- src/Common/FieldWithErrorMessage.test.tsx
+++ src/Common/FieldWithErrorMessage.test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import FieldWithErrorMessage from "./FieldWithErrorMessage";
 import renderer from "react-test-renderer";
 
@@ -25,5 +25,29 @@ describe(`The Field with Error Message component`, () => {
       .toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  it(`Should have error bootstrap classes applied when input has error and was touched`, () => {
+    const name = "email";
+    const value = "patryk.pl";
+    const onChange = (): void => {};
+    const onBlur = (): void => {};
+    const touch = true;
+    const errorMessage = "Invalid email address";
+
+    const tree = renderer.create(
+      <FieldWithErrorMessage
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        touch={touch}
+        error={errorMessage}
+      />
+    );
+
+    const errorDiv = tree.root.findByProps({ className: "invalid-feedback" });
+    expect(errorDiv).not.toBeNull();
+    expect(errorDiv.children[0]).toEqual(errorMessage);
   });
 });
