220c11dc8a54f423d6b676f96549c2f4ccc24a08
Patryk Rubaj <patryk.rubaj@gmail.com>
2020-10-01 16:16:00 +0200

    Configured jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   reporters: ["default", "jest-junit"],
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.jest.json",
+    },
+  },
 };
