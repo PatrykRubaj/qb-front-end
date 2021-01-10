a98d7579b3f13de02e2ee6b2489fde90ec4c9e69
Patryk Rubaj <patryk.rubaj@gmail.com>
2021-01-10 10:40:57 +0100

    More reorg

---
+++ src/quantumbudget-api/QuantumBudget.API/Controllers/BudgetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuantumBudget.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BudgetController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return Ok(new { id });
+        }
+
+        [HttpGet]
+        public ActionResult<> Generate(Budget input)
+        {
+            
+        }
+    }
+}
