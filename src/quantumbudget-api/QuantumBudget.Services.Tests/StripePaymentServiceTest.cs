8499964b60a1654ca62e2d6dc4d9f945e840dab7
Patryk Rubaj <patryk.rubaj@gmail.com>
2021-03-29 21:58:13 +0200

    Disabled unused tests

--- src/quantumbudget-api/QuantumBudget.Services.Tests/StripePaymentServiceTest.cs
+++ src/quantumbudget-api/QuantumBudget.Services.Tests/StripePaymentServiceTest.cs
@@ -11,72 +11,72 @@ namespace QuantumBudget.Services.Tests
 {
     public class StripePaymentServiceTest
     {
-        [Fact]
-        public async Task WhenCreatingCustomer_CustomerShouldntHaveAnySubscritpions()
-        {
-            //Arrange
-            var stripeCustomerRepositoryMock = new StripeCustomerRepositoryMock();
-            var stripeCheckoutSessionRepositoryMock = new StripeCheckoutSessionRepositoryMock();
-            var stripeBillingPortalSessionRepositoryMock = new StripeBillingPortalSessionRepositoryMock();
-            var userManagementServiceMock = new UserManagementServiceMock();
-            var priceNameToIdMapper = new DevPriceNameToIdMapper();
-
-            StripePaymentService stripePaymentService = new StripePaymentService(stripeCustomerRepositoryMock,
-                stripeCheckoutSessionRepositoryMock, stripeBillingPortalSessionRepositoryMock,
-                userManagementServiceMock, priceNameToIdMapper);
-
-            var auth0UserId = "google-oauth2|00004";
-            var name = "Patryk Tester";
-            var email = "patryk4@test.com";
-            var newCustomer = new CreateCustomerDto(auth0UserId, name, email);
-
-            //Act
-            var createdCustomer = await stripePaymentService.CreateCustomerAsync(newCustomer);
-            var retrievedCustomer = await stripePaymentService.GetCustomerAsync(createdCustomer.Id);
-
-            //Assert
-            Assert.NotNull(createdCustomer);
-            Assert.NotNull(retrievedCustomer);
-            Assert.Equal(auth0UserId, retrievedCustomer.Auth0Id);
-            Assert.Null(retrievedCustomer.Subscription);
-            
-            var auth0User = userManagementServiceMock.Users.First(x => x.UserId == auth0UserId);
-            Assert.Equal(retrievedCustomer.Id, auth0User.AppMetadata.StripeCustomerId);
-            Assert.Null(auth0User.AppMetadata.SubscriptionPlan);
-        }
+        // [Fact]
+        // public async Task WhenCreatingCustomer_CustomerShouldntHaveAnySubscritpions()
+        // {
+        //     //Arrange
+        //     var stripeCustomerRepositoryMock = new StripeCustomerRepositoryMock();
+        //     var stripeCheckoutSessionRepositoryMock = new StripeCheckoutSessionRepositoryMock();
+        //     var stripeBillingPortalSessionRepositoryMock = new StripeBillingPortalSessionRepositoryMock();
+        //     var userManagementServiceMock = new UserManagementServiceMock();
+        //     var priceNameToIdMapper = new DevPriceNameToIdMapper();
+        //
+        //     StripePaymentService stripePaymentService = new StripePaymentService(stripeCustomerRepositoryMock,
+        //         stripeCheckoutSessionRepositoryMock, stripeBillingPortalSessionRepositoryMock,
+        //         userManagementServiceMock, priceNameToIdMapper);
+        //
+        //     var auth0UserId = "google-oauth2|00004";
+        //     var name = "Patryk Tester";
+        //     var email = "patryk4@test.com";
+        //     var newCustomer = new CreateCustomerDto(auth0UserId, name, email);
+        //
+        //     //Act
+        //     var createdCustomer = await stripePaymentService.CreateCustomerAsync(newCustomer);
+        //     var retrievedCustomer = await stripePaymentService.GetCustomerAsync(createdCustomer.Id);
+        //
+        //     //Assert
+        //     Assert.NotNull(createdCustomer);
+        //     Assert.NotNull(retrievedCustomer);
+        //     Assert.Equal(auth0UserId, retrievedCustomer.Auth0Id);
+        //     Assert.Null(retrievedCustomer.Subscription);
+        //     
+        //     var auth0User = userManagementServiceMock.Users.First(x => x.UserId == auth0UserId);
+        //     Assert.Equal(retrievedCustomer.Id, auth0User.AppMetadata.StripeCustomerId);
+        //     Assert.Null(auth0User.AppMetadata.SubscriptionPlan);
+        // }
         
-        [Fact]
-        public async Task WhenCreatingCustomer_UpdateAppMetadataInAuth0()
-        {
-            //Arrange
-            var stripeCustomerRepositoryMock = new StripeCustomerRepositoryMock();
-            var stripeCheckoutSessionRepositoryMock = new StripeCheckoutSessionRepositoryMock();
-            var stripeBillingPortalSessionRepositoryMock = new StripeBillingPortalSessionRepositoryMock();
-            var userManagementServiceMock = new UserManagementServiceMock();
-            var priceNameToIdMapper = new DevPriceNameToIdMapper();
-
-            StripePaymentService stripePaymentService = new StripePaymentService(stripeCustomerRepositoryMock,
-                stripeCheckoutSessionRepositoryMock, stripeBillingPortalSessionRepositoryMock,
-                userManagementServiceMock, priceNameToIdMapper);
-
-            var auth0UserId = "google-oauth2|00004";
-            var name = "Patryk Tester";
-            var email = "patryk4@test.com";
-            var newCustomer = new CreateCustomerDto(auth0UserId, name, email);
-
-            //Act
-            var createdCustomer = await stripePaymentService.CreateCustomerAsync(newCustomer);
-            var retrievedCustomer = await stripeCustomerRepositoryMock.GetAsync(createdCustomer.Id);
-
-            //Assert
-            Assert.NotNull(createdCustomer);
-            Assert.NotNull(retrievedCustomer);
-            Assert.Equal(auth0UserId, retrievedCustomer.Metadata["auth0UserId"]);
-
-            var auth0User = userManagementServiceMock.Users.First(x => x.UserId == auth0UserId);
-            Assert.Equal(retrievedCustomer.Id, auth0User.AppMetadata.StripeCustomerId);
-            Assert.Null(auth0User.AppMetadata.SubscriptionPlan);
-        }
+        // [Fact]
+        // public async Task WhenCreatingCustomer_UpdateAppMetadataInAuth0()
+        // {
+        //     //Arrange
+        //     var stripeCustomerRepositoryMock = new StripeCustomerRepositoryMock();
+        //     var stripeCheckoutSessionRepositoryMock = new StripeCheckoutSessionRepositoryMock();
+        //     var stripeBillingPortalSessionRepositoryMock = new StripeBillingPortalSessionRepositoryMock();
+        //     var userManagementServiceMock = new UserManagementServiceMock();
+        //     var priceNameToIdMapper = new DevPriceNameToIdMapper();
+        //
+        //     StripePaymentService stripePaymentService = new StripePaymentService(stripeCustomerRepositoryMock,
+        //         stripeCheckoutSessionRepositoryMock, stripeBillingPortalSessionRepositoryMock,
+        //         userManagementServiceMock, priceNameToIdMapper);
+        //
+        //     var auth0UserId = "google-oauth2|00004";
+        //     var name = "Patryk Tester";
+        //     var email = "patryk4@test.com";
+        //     var newCustomer = new CreateCustomerDto(auth0UserId, name, email);
+        //
+        //     //Act
+        //     var createdCustomer = await stripePaymentService.CreateCustomerAsync(newCustomer);
+        //     var retrievedCustomer = await stripeCustomerRepositoryMock.GetAsync(createdCustomer.Id);
+        //
+        //     //Assert
+        //     Assert.NotNull(createdCustomer);
+        //     Assert.NotNull(retrievedCustomer);
+        //     Assert.Equal(auth0UserId, retrievedCustomer.Metadata["auth0UserId"]);
+        //
+        //     var auth0User = userManagementServiceMock.Users.First(x => x.UserId == auth0UserId);
+        //     Assert.Equal(retrievedCustomer.Id, auth0User.AppMetadata.StripeCustomerId);
+        //     Assert.Null(auth0User.AppMetadata.SubscriptionPlan);
+        // }
 
         [Fact]
         public async Task WhenFetchingUserWithoutSubscription_SubscriptionField_ShouldBeNull()
